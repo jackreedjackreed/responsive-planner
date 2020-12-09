72c17eed92cb49d12088d2043a8f98cc750c568f
J-Boogie <jackr353@gmail.com>
2020-12-09 10:13:26 -0600

    put local time in the title

--- script.js
+++ script.js
@@ -2,9 +2,12 @@
 var currentDay = $("#currentDay");
 var now = luxon.DateTime.local();
 console.log(now);
-var titleTime = luxon.DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' });
+var titleTime = luxon.DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit', hour: "2-digit", minute: "2-digit"});
 console.log(titleTime);
 var workdayHours = [06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22];
+
+// get current time and put it below the jumbotron
+$("#currentDay").text(titleTime);
 
 // populate more than one hour
 for (i=0; i<workdayHours.length; i++) {
@@ -22,7 +25,7 @@ for (i=0; i<workdayHours.length; i++) {
     // current = red
     // upcoming = green
 
-// get current time and put it below the jumbotron
+
 
 // click right column button to edit middle column texts
     // once to edit
