433c62d351f60d2f7b1e1872e25df9160bb69d5d
J-Boogie <jackr353@gmail.com>
2020-12-09 15:24:28 -0600

    colorize the time-block class

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
 
 var currentDay = $("#currentDay");
 var now = luxon.DateTime.local();
-console.log(now);
+//console.log(now);
+var nowHour = luxon.DateTime.local().toObject().hour;
+//console.log(nowHour);
 var titleTime = luxon.DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit', hour: "2-digit", minute: "2-digit"});
-console.log(titleTime);
-var workdayHours = [06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22];
+//console.log(titleTime);
+var workdayHours = [09, 10, 11, 12, 13, 14, 15, 16, 17];
 
 // get current time and put it below the jumbotron
 $("#currentDay").text(titleTime);
 
-// apply timeblock class
-//$("#timeblock").addClass("time-block");
-
-
 
 // show events in each time block
 
+    // make time blocks clickable 
+
+    // upon clicking save button, user input gets saved to local storage
+
 // color code blocks according to time
-    // past = grey
-    // current = red
-    // upcoming = green
+function updateColors (currentTimeHours) {
+    $(".time-block").each(function () {
+        // go through all elements w/ time-block class and check id against currentTimeHours
+        if ($(this).attr("id") < currentTimeHours) {
+            // past = grey
+            $(this).addClass("past");
+        }
+        else if ($(this).attr("id") == currentTimeHours) {
+            // current = red
+            $(this).addClass("present");
+        }
+        else {
+            // future = green
+            $(this).addClass("future");
+        }
+    })
+}
 
-// time-row
-// event-row
-// edit-row
+updateColors(nowHour);
+    
 
 
-// click right column button to edit middle column texts
-    // once to edit
-    // again to save to local storage
 
