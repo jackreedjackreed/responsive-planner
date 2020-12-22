c7974f68308fff67d84bedad42663eedf705bf55
J-Boogie <jackr353@gmail.com>
2020-12-22 11:27:36 -0600

    clean up and comment in logic

--- script.js
+++ script.js
@@ -46,92 +46,34 @@ updateColors(nowHours);
 
 // make buttons clickable and get parameters for saveToLS fxn
 saveBtns.on("click", function(){
-    // gets the hour of the event of the inputText
+    // gets the hours of the event of the inputText
     var eventHour = $(this).parent().attr("id") + ":00";
-    //console.log(eventHour);
     // gets the value of the input text in the text area
     var inputText = $(this).prev().val();
-    //console.log(inputText);
-
-    // trying to save in this fxn instead of saveToLS fsn
+    // set local storage
     localStorage.setItem(eventHour, inputText);
     
-    // saveToLS(eventHour, inputText);
 
 });
 
 function updateEvents() {
     var timeBlockHours = $(".hour");
+    // loop through hours
     timeBlockHours.each(function() {
-        var eventHour = $(this).text()
-        // console.log(eventHour);
+        // get string eventHour
+        var eventHour = $(this).text();
+        // get storedEvent of each eventHour
         var storedEvent = localStorage.getItem(eventHour);
-        console.log($(this).siblings());
-        // console.log(eventHour + storedEvent + " = stored event from storage!");
-        // console.log(storedEvent);
+        // check if var storedEvent is not empty, if not --> 
+        // set textarea sibling to the value of eventHour in storage
         if (storedEvent !== null) {
-            // console.log("not null");
             $(this).siblings(0).val(storedEvent);
-            
+             }
         }
-        // ^^ all comes back null and i'm not sure why. this is where i got stuck. 
-        // THE ONLY THING i can't figure out is how to retrieve and populate the boxes that have stuff in local storage.
-        // if (storedEvent !== null) {
-            // console.log("not null!");
-            // timeBlocks.children([1]).val(storedEvent);
-        }
-    // // }
 )};
 
-// update the events
+// update the events, called upon load/refresh
  updateEvents()
-
-// var arrayOfKeys = Object.keys(localStorage);
-// // console.log(arrayOfKeys);
-// var arrayOfValues = Object.values(localStorage);
-// // console.log(arrayOfValues);
-
-// // save to local storage function
-// function saveToLS(eventHour, userInput) {
-//     localStorage.setItem(eventHour, userInput);
-//     // console.log(eventHour);
-//     // console.log(userInput);
-// }
-
-// function updateEvents2() {
-//     var timeBlocks = $(".time-block");
-//     // console.log(timeBlocks + "timeblocks");
-//     var timeBlocksID = timeBlocks.attr("id");
-//     // console.log(timeBlocksID + "timeblocksid");
-//     var timeBlockTextArea = timeBlocks.children([1]);
-//     //var timeBlockTextArea = $(".schedule");
-//     // console.log(timeBlockTextArea + "timeBlockTextArea");
-
-//     for (i = 0; i < arrayOfKeys.length; i++) {
-//         var valuess = arrayOfValues[i];
-//         if (arrayOfKeys[i] !== null) {
-//             storedEvent = localStorage.getItem(workHours[i]);
-//             // console.log(storedEvent);
-//             timeBlockTextArea.val(storedEvent);
-//             // I ALSO THOUGHT this would work, but it doesnt. Would love an explanation for either function.
-//         }
-//     }
-// }
-// updateEvents2();
-
-
-// var timeBlocks = $(".time-block");
-//     //console.log(timeBlocks + "timeblocks");
-//     var timeBlocksID = timeBlocks.attr("id");
-//     //console.log(timeBlocksID + "timeblocksid");
-//     var timeBlockTextArea = timeBlocks.children[1];
-//     //console.log(timeBlockTextArea + "timeBlockTextArea");
-//     var storedEvent = localStorage.getItem(timeBlocksID)
-//     //console.log(storedEvent + "storedevent");
-//     // here, the stored event comes out correctly --> the correct hour/ID has the correct stored event
-//     var timeBlockHours = $(".hour")
-//     //console.log(timeBlockHours + "timeblockhours");
-
 
 
     
